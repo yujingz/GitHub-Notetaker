463f2e35fe65a6d882ba7fa69bd2155c1de73ad7
Yujing Zheng <yujingz@me.com>
2015-04-22 22:49:51 -0500

    init

---
+++ index.ios.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var Main = require('./App/Components/Main.js');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+} = React;
+
+class githubNotetaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'GitHub Notetaker',
+          component: Main,
+        }}
+      />
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
